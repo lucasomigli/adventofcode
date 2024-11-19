a27e3258d8b34c773544b99c6c20e34d6ddc0e90
Luca <somigliluca@gmail.com>
2024-11-19 19:11:09 +0000

    part 1 solution day 12 2020

--- day12/day12.h
+++ day12/day12.h
@@ -61,4 +61,70 @@ int Solver::Solve_day_12_part1() {
   return std::abs(x) + std::abs(y);
 }
 
+struct Ship {
+  int x = 0;
+  int y = 0;
+};
+
+struct Waypoint {
+  int x = 0;
+  int y = 0;
+};
+
+int Solver::Solve_day_12_part2() {
+  std::string line;
+  char direction;
+  int movement;
+  int rotation = 0;
+
+  Ship ship;
+  Waypoint waypoint;
+  waypoint.x = ship.x + 10;
+  waypoint.y = ship.y + 1;
+
+  while (std::getline(file, line)) {
+    direction = line[0];
+    line.erase(0, 1);
+    movement = std::stoi(line);
+    std::cout << movement << " " << direction << std::endl;
+
+    switch (direction) {
+      case 'N':
+        waypoint.y += direction;
+        break;
+      case 'S':
+        waypoint.y -= movement;
+        break;
+      case 'E':
+        waypoint.x += movement;
+        break;
+      case 'W':
+        waypoint.x -= movement;
+        break;
+      case 'L':
+        rotation = (rotation - movement + 360) % 360;
+        break;
+      case 'R':
+        rotation = (rotation + movement) % 360;
+        break;
+      case 'F':
+        int distanceX = waypoint.x - ship.x;
+        int distanceY = waypoint.y - ship.y;
+        //        std::cout << "DEBUG" << movement * (waypoint.x - ship.x) <<
+        //        std::endl;
+        ship.x += movement * (waypoint.x - ship.x);
+        ship.y += movement * (waypoint.y - ship.y);
+        waypoint.x = ship.x + distanceX;
+        waypoint.y = ship.y + distanceY;
+        break;
+    }
+
+    std::cout << "Direction: " << direction << " Movement: " << movement
+              << " ship.x: " << ship.x << " ship.y: " << ship.y
+              << " waypoint.x: " << waypoint.x << " waypoint.y: " << waypoint.y
+              << std::endl;
+  }
+  return 1;
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ int main() {
   std::cout << "Using version " << __cplusplus << std::endl;
 
   std::string AOC_day = "day12";
-  std::cout << "AdventOfCode Solution for day: " << AOC_day << std::endl;
+  std::cout << "AdventOfCode Solution for: " << AOC_day << std::endl;
   std::filesystem::path currentPath = std::filesystem::current_path();
 
   std::string solutionPath = currentPath.generic_string() + "/" + AOC_day;
   std::string inputFile = solutionPath + "/input.txt";
   std::string testFile = solutionPath + "/test.txt";
 
-  Solver solver(inputFile);
-  int solution = solver.Solve_day_12_part1();
+  Solver solver(testFile);
+  int solution = solver.Solve_day_12_part2();
   std::cout << "The Solution is: " << solution << std::endl;
 
   return 0;
